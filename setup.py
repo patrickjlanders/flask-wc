e3298b3943b2d6d59e44207cd3c2edf79722c73c
Patrick Landers <patrickjlanders@yahoo.co.uk>
2019-04-11 00:23:22 +0100

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(name='flask-by-example',
+      version='1.0',
+      packages=find_packages(),
+      scripts=['manage.py'] # puts manage.py on PATH of virtualenv, so can run manage.py runserver from anywhere
+      )
